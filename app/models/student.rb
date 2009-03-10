b3a7daf97ead1417b7a6eb83535f014b7b58dfee
Aaron Unger <aunger@concord.org>
2009-03-10 11:54:58 -0400

    Updated student and teacher models so that they only return teachers or students.

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,22 @@ class Student < ActiveRecord::Base
   has_many :class_students, :foreign_key => :member_id
   
   has_many :courses, :through => :class_students
+  
+  # Filter all members by member_type
+  def self.find(*args)
+    if args.first == :all || args.first == :first
+      saw_conditions = false
+      args.each do |a|
+        if a.kind_of?(Hash) && a[:conditions]
+          saw_conditions = true
+          a[:conditions][:member_type] = 'student'
+        end
+      end
+      if ! saw_conditions
+        a = {:conditions => {:member_type => 'student'}}
+        args.push a
+      end
+    end
+    super
+  end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,4 +3,22 @@ class Teacher < ActiveRecord::Base
   set_primary_key :member_id
   
   has_many :courses, :foreign_key => :class_teacher
+  
+  # Filter all members by member_type
+  def self.find(*args)
+    if args.first == :all || args.first == :first
+      saw_conditions = false
+      args.each do |a|
+        if a.kind_of?(Hash) && a[:conditions]
+          saw_conditions = true
+          a[:conditions][:member_type] = ['teacher','admin','superuser']
+        end
+      end
+      if ! saw_conditions
+        a = {:conditions => {:member_type => ['teacher','admin','superuser']}}
+        args.push a
+      end
+    end
+    super
+  end
 end
